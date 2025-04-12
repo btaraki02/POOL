03bb0358b577a8640e2d59a8befa51c63e2d563e
Taraki Bilal <your.email@example.com>
2025-04-12 11:21:00 +0100

    T

--- C00/ex06.c
+++ C00/ex06.c
@@ -1,31 +1,24 @@
 #include <unistd.h>
-/*void ft_print_comb2(void)
+void ft_putchar(char c)
 {
-   int a = '0';
-   int b = '0';
-   while(a <= '99')
-   {
-    b = '0';
-    while(b <= '98')
+    write(1 , &c , 1);
+}
+void ft_print_comb2(void)
+{
+    int a = '0';
+    int b = '0';
+    while(a <= '99')
     {
-        
-                write(1 , &a ,1);
-                write(1 , & ,1);
-                write(1 ," ",1);
-                write(1 , &c ,1);
-                write(1 , &d ,1);
-                if(!(a == '9' && b == '8' && c == '9' && d == '9'))
-                {
-                    write(1 ," ,",2);
-                }
-                d++;
-            }
-            c++;
+        b = '0';
+        while(b <= '98')
+        {
+            ft_putchar(&a);
+            ft_putchar(' ');
+            ft_putchar(&b);
+            b++;
         }
-        b++;
+        a++;
     }
-    a++;
-   }
 }
 int main()
 {

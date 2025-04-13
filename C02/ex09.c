f4328485f325a0aec0e1a45f2b37acd6f1b0c4d8
Taraki Bilal <your.email@example.com>
2025-04-13 10:36:51 +0100

    !ok

--- C02/ex09.c
+++ C02/ex09.c
@@ -1,33 +1,41 @@
-#include <stdio.h>
+//#include <stdio.h>
 #include <unistd.h>
 char *ft_strcapitalize(char *str)
 {
     int i = 0;
+    int new_Word_ = 1;
     while(str[i] != '\0')
     {
-        if(str[i] <= 'A' && str[i] >= 'Z')
+        if(str[i] >= 'a' && str[i] <= 'z')
         {
-            str[i] +=32;
-        }
-        i++;
-    }
-    i = 0;
-    while(str[i] != '\0')
-    {
-        if(str[i] == '+' || str[i] == '-' || str[i] == ' ')
-        {
-            if(str[i] <= 'z' && str[i] >= 'a')
+            if(new_Word_)
             {
-            str[i + 1] -=32;
+                str[i] -=32;
             }
         }
-        i++;
+        else if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            if(!new_Word_)
+            {
+                str[i] +=32;
+            }
+        }
+        if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))
+            {
+                new_Word_ = 0;
+            }
+            else
+            {
+                new_Word_ = 1;
+            }
+            i++;
     }
-    return (str);
+    return str;
+   
 }
-int main()
+/*int main()
 {
     char array[] = " salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
     ft_strcapitalize(array);
     printf("%s\n",array);
-}
+}*/

6cd48e9a8070443e8fc7ba1fee7ef8bc21810f85
Taraki Bilal <your.email@example.com>
2025-04-14 11:57:11 +0100

    OL!

---
+++ C03/ex03.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+//#include <stdio.h>
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+    char *start = dest;
+    while(*dest != '\0')
+    {
+        dest++;
+    }
+    while(*src != '\0' && nb > 0)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+        nb--;
+    }
+    *dest = '\0';
+    return (start);
+}
+/*int main()
+{
+    char array[100] = "HI I AM BILAL ";
+    char skl[50] = "TARAKI I AM A MORO";
+    unsigned int nb = 9;
+    ft_strncat(array, skl,nb);
+    printf("%s\n",array);
+}*/

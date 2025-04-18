36f84de9ec219bcb5d1210b766414a745764579e
Taraki Bilal <your.email@example.com>
2025-04-18 10:15:30 +0100

    nice

--- C03/ex05.c
+++ C03/ex05.c
@@ -1,27 +1,38 @@
-/*#include <stdio.h>
+//#include <stdio.h>
 #include <unistd.h>
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
     unsigned int i = 0;
-    while(*dest != '\0')
-    {
-        dest++;
-    }
-    while(*src != '\0' && i < size -1)
-    {
-        *dest = src[i];
-        dest++;
-        i++;
-    }
-    while(src[i] != '\0')
+    unsigned int j = 0;
+    unsigned int res = 0;
+    while(dest[i] != '\0')
     {
         i++;
     }
-    return i;
+    while(src[res] != '\0')
+    {
+        res++;
+    }
+    if(size <= i)
+    {
+        res += size;
+    }
+    else
+    {
+        res += i;
+    }
+    while(src[j] != '\0' && i < size -1)
+    {
+        dest[i] = src[j];
+        j++;
+        i++;
+    }
+        dest[i] = '\0';
+    return res;
 }
-int main()
+/*int main()
 {
-    char dest[30] = " HI";
+    char dest[7] = " HI";
     char src[] = " BILAL TARAKI ";
     unsigned  int i = ft_strlcat(dest,src,30);
     printf("%s\n",dest);

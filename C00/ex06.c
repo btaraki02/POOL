b81a805296ad649c69f673ccaed14db1122f1483
Taraki Bilal <your.email@example.com>
2025-04-20 06:55:57 +0100

    gg

--- C00/ex06.c
+++ C00/ex06.c
@@ -1,26 +1,38 @@
 #include <unistd.h>
-void ft_putchar(char c)
+void ft_putchar_c(char c)
 {
-    write(1 , &c , 1);
+    write(1 , &c, 1);
+}
+int ft_number(char c , int n)
+{
+    int rest = 0;
+    if(n == 0){
+        rest = c / 10 + '0';
+    }
+    else {
+        rest = c % 10 + '0';
+    }
+    return rest;
 }
 void ft_print_comb2(void)
 {
-    int a = '0';
-    int b = '0';
-    while(a <= '99')
+    int a = 0;
+    int b ;
+    while(a <= 98)
     {
-        b = '0';
-        while(b <= '98')
+        b = a + 1;
+        while( b <= 99)
         {
-            ft_putchar(&a);
-            ft_putchar(' ');
-            ft_putchar(&b);
-            b++;
+            ft_putchar_c(ft_number(a ,0));
+            ft_putchar_c(ft_number(a ,1));
+            ft_putchar_c(' ');
+            ft_putchar_c(ft_number(b ,0));
+            ft_putchar_c(ft_number(b ,1));
+            if(!(a == 98 && b == 99))
+            write(1 ,", ",2);
+        b++;
+
         }
         a++;
     }
-}
-int main()
-{
-    ft_print_comb2();
 }

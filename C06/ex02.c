d8c57945aaec733b825002b4142255972a4361bd
btaraki02 <btaraki056@gmail.com>
2025-04-24 10:48:58 +0200

    ok

---
+++ C06/ex02.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+void ft_putchar(char c)
+{
+    write(1 , &c , 1);
+}
+int main(int argc , char* argv[])
+{
+    int i = argc -1;//'\0'!!!!!
+    int j = 0;
+    while(i > 0)
+    { 
+        j = 0;
+        while(argv[i][j] != '\0')
+        {
+            ft_putchar(argv[i][j]);
+            j++;
+        }
+        ft_putchar('\n');
+        i--;
+    }
+    
+    return 0;
+}

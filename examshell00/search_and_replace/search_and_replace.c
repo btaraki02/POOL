2f4f70a64b69c9e6f3aa6cfdd81431ee01556ada
btaraki02 <btaraki056@gmail.com>
2025-05-12 13:34:17 +0200

    🥳 Congratulation! You have finished the Exam Rank 01 !

---
+++ examshell00/search_and_replace/search_and_replace.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_search_and_replace(char *str, char c, char d)
+{
+	int	i = 0;
+
+	while (str[i])
+	{
+		if (str[i] == c)
+			str[i] = d;
+		i++;
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4 && argv[2][1] == '\0' && argv[3][1] == '\0')
+	{
+		ft_search_and_replace(argv[1], argv[2][0], argv[3][0]);
+		ft_putstr(argv[1]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}

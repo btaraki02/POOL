e862fa9738614e780c8272fd2afbdc04638dda9f
btaraki02 <btaraki056@gmail.com>
2025-05-20 21:57:46 +0200

    🐦‍🔥​

--- C00/ex07.c
+++ C00/ex07.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
-void ft_put(char c)
+void ft_putchr(char c)
 {
-    write(1 , &c ,1 );
+    write(1 , &c , 1); 
 }
 void ft_putnbr(int nb)
 {
-    if(nb < 0)
-    {
-        ft_put('-');
-        nb *= -1;
-    }
     if(nb == -2147483648)
     {
-        write(1 , "2147483648" , 12);
-        return ;
+        write(1 , "-2147483648" ,11);
+        return;
     }
-    if(nb >= 10) //this is E
+    if(nb < 0)
     {
-        ft_putnbr(nb / 10);
-        nb %= 10;
+        ft_putchr('-');
+        nb = -nb;
     }
-    ft_put(nb + '0');
+    if(nb > 9)
+    {
+        ft_putnbr(nb /10);
+    }
+    ft_putchr(nb % 10 + '0');
 }

6152097f03cdd2fd6300b1db3ecccfd98ae7d8c4
Taraki Bilal <btaraki056@gmail.com>
2025-05-20 16:59:07 +0200

    Update ex04.c

--- C03/ex04.c
+++ C03/ex04.c
@@ -2,28 +2,8 @@
 #include <unistd.h>
 char *ft_strstr(char *str, char *to_find)
 {
-    if (!*to_find)
-    return str;
- while(*str != '\0')
- {
-    char *start = str;
-    char *temp_find = to_find;
-    if(*str == *to_find)
-    {
-        while(*temp_find != '\0' &&  *temp_find == *start )
-        {
-            temp_find++;
-            start++;
-        }
-        if(*temp_find == '\0')
-        {
-            return str;
-        }
-    }
-    str++;
- }
- return NULL;
-}
+    if(!to_find[0])
+        return str;
 /*int main()
 {
     char array[] = "HI I AM BILAL TARAKI I AM A MORO";

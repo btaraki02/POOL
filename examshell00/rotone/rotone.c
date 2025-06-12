0726ff25a589fd33591a77b5cf6963f9b22980bb
btaraki02 <btaraki056@gmail.com>
2025-06-12 22:31:24 +0200

    nice

--- examshell00/rotone/rotone.c
+++ examshell00/rotone/rotone.c
@@ -12,28 +12,25 @@
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void rotone(char *str)
 {
-	write(1, &c, 1);
+    while (*str)
+    {
+        if ((*str >= 'a' && *str <= 'y') || (*str >= 'A' && *str <= 'Y'))
+            *str += 1;
+        else if (*str == 'z')
+            *str = 'a';
+        else if (*str == 'Z')
+            *str = 'A';
+        write(1, str, 1);
+        str++;
+    }
 }
 
-int		main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (argc == 1 || argc > 2)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	while (*argv[1])
-	{
-		if (*argv[1] >= 'a' && *argv[1] <= 'z')
-			ft_putchar(*argv[1] == 'z' ? 'a' : *argv[1] + 1);
-		else if (*argv[1] >= 'A' && *argv[1] <= 'Z')
-			ft_putchar(*argv[1] == 'Z' ? 'A' : *argv[1] + 1);
-		else
-			ft_putchar(*argv[1]);
-		argv[1]++;
-	}
-	write(1, "\n", 1);
-	return (0);
+    if (argc == 2)
+        rotone(argv[1]);
+    write(1, "\n", 1);
+    return 0;
 }
